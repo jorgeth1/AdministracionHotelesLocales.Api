43ce486dfe32fa35dcd06241c1591527c301602d
Jorge Tapias <jorge1tapias@gmail.com>
2023-12-07 14:51:08 -0500

    ultima migracion y se agrega dockerfile

---
+++ AdministracionHotelesLocales.Infra/Migrations/20231207194753_ReservaNewProperties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdministracionHotelesLocales.Infra.Migrations
+{
+    public partial class ReservaNewProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaSalida",
+                schema: "AdministracionHotelesLocales",
+                table: "Reservas",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaSalida",
+                schema: "AdministracionHotelesLocales",
+                table: "Reservas");
+        }
+    }
+}
